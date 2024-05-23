88c3fd8aaa27594a49bb3bfafe6fca2c1c510794
Mia Gobble <96698128+MiaGobble@users.noreply.github.com>
2024-05-22 20:21:57 -0500

    Create BuildCommands.lua

---
+++ src/FigmaImportAssistant/Main/Interface/Keybinds/BuildCommands.lua
@@ -0,0 +1,9 @@
+local Utility = require(script.Parent.Parent.Parent.Utility)
+--local Interface = require(script.Parent.Parent.Interface)
+local Keybind = require(script.Parent.Keybind)
+
+return function()
+    --Keybind.new("ceo", {}):Bind(function()
+        -- Center object
+    --end)
+end
